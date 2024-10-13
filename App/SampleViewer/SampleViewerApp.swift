d9efa80797844fadeaf1069e1bfe14a005ae6682
S-Shimotori <gingerale.jagajaga@gmail.com>
2024-10-13 14:20:10 +0900

    Add an iOS project

---
+++ App/SampleViewer/SampleViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleViewerApp.swift
+//  SampleViewer
+//
+//  Created by S-Shimotori on 2024/10/13.
+//
+
+import SwiftUI
+
+@main
+struct SampleViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App/SampleViewerTests/SampleViewerTests.swift
@@ -0,0 +1,17 @@
+//
+//  SampleViewerTests.swift
+//  SampleViewerTests
+//
+//  Created by S-Shimotori on 2024/10/13.
+//
+
+import Testing
+@testable import SampleViewer
+
+struct SampleViewerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
