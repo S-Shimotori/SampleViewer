18ef89becf5f5f4423256ae1b7955b6c5ba3692f
S-Shimotori <gingerale.jagajaga@gmail.com>
2024-10-13 15:04:32 +0900

    Add a sample library

--- Package.swift
+++ Package.swift
@@ -8,17 +8,17 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "SampleViewer",
-            targets: ["SampleViewer"]),
+            name: "SampleLibrary",
+            targets: ["SampleLibrary"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SampleViewer"),
+            name: "SampleLibrary"),
         .testTarget(
-            name: "SampleViewerTests",
-            dependencies: ["SampleViewer"]
+            name: "SampleLibraryTests",
+            dependencies: ["SampleLibrary"]
         ),
     ]
 )
